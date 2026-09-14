beb3fd192e794b5f68dc6022dd3515acd3121820
M. Ichwan Al Hafidz <m.ichwan.al.hafizd24092@mhs.uingusdur.ac.id>
2026-09-14 14:06:09 +0700

    feat: tambah interface DataCuaca dan WeatherCardProps

---
+++ types/cuaca.ts
@@ -0,0 +1,15 @@
+export interface DataCuaca {
+  kota: string;
+  suhu: number;
+  kelembapan: number;
+  catatan?: string;
+}
+
+export type TingkatAQI =
+  "BAIK" | "SEDANG" | "TIDAK_SEHAT" | "BERBAHAYA";
+
+export interface WeatherCardProps {
+  kota: string;
+  suhu: number;
+  tingkatAQI: TingkatAQI;
+}
